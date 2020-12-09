8bbb4072565f4ad4b3af634731bf9f5b9b8b284b
Wenbo Jiang <jwb0324@gmail.com>
2020-12-08 22:15:11 -0500

    modified main

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ camera = PiCamera()
 
 # set the camera to the max resolution
 # camera.resolution = (2592, 1944)
-camera.resolution = (2000, 980)
+camera.resolution = (1800, 800)
 camera.framerate = 15
 
 # start taking picture
